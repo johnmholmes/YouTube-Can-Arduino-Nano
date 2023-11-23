0124f33ee3b2253a368042bb79b04f8da053b221
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2023-11-23 14:37:49 +0000

    Update can_send.ino.ino

--- can_send.ino/can_send.ino.ino
+++ can_send.ino/can_send.ino.ino
@@ -1,6 +1,21 @@
 #include <SPI.h>
 #include <mcp_can.h>
 
+/*
+This sketch is for testing an Arduino Nano using the MCP2515 module using spi
+
+Pins used for connecting the module are.
+PIN 2 = INT
+PIN 10 = CS
+PIN 11 = SI
+PIN 12 = SO
+PIN 13 = SCK
+
+GND and 5 volts also required.
+
+The module sends 2 messages on the can bus of "on or of" 
+*/
+
 const int SPI_CS_PIN = 10;  // Chip Select pin for MCP2515
 const int CAN_INT_PIN = 2; // Interrupt pin for MCP2515
 
