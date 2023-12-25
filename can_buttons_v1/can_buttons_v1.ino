ebd271e4e1c22b94fbc6690caf462a9346869576
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2023-12-25 21:21:51 +0000

    Update can_buttons_v1.ino

--- can_buttons_v1/can_buttons_v1.ino
+++ can_buttons_v1/can_buttons_v1.ino
@@ -17,7 +17,7 @@ void setup() {
   pinMode(PIN_7, INPUT_PULLUP);
 
   // Initialize MCP2515
-  if (CAN.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ) == CAN_OK) {
+  if (CAN.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK) {
     Serial.println("MCP2515 Initialized Successfully!");
     CAN.setMode(MCP_NORMAL);
   } else {
